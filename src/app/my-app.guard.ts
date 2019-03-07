ffba7a143075b1366b4b0137a7ce2bfeb51b0c35
Giorgi Ptskialadze <giopcqialadze@gmail.com>
2019-03-07 20:18:58 +0400

    add guard

--- src/app/my-app.guard.ts
+++ src/app/my-app.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-
+import {firebaseService} from './firebase/firebase.service';
 @Injectable({
   providedIn: 'root'
 })
 export class MyAppGuard implements CanActivate, CanActivateChild, CanLoad {
+  loadStatus:boolean;
+  constructor(private fb:firebaseService){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if(window.localStorage.getItem('activate')){
+      this.loadStatus = true;
+    }
+    else {
+      this.loadStatus = false;
+    }
+    this.fb.canAcitaveSubjet.subscribe((status:boolean)=>{
+      this.loadStatus = status;
+    })
+      return this.loadStatus;
   }
   canActivateChild(
     next: ActivatedRouteSnapshot,
